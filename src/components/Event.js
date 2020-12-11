cf6e803f5fa7d847e8868baafe25311be749b8ce
ConArrr <conarrr7714@gmail.com>
2020-12-11 07:32:32 +0000

    replace type names with constants

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,11 @@
+import { DELETE_EVENT } from '../actions/'
+
 const Event = ({ dispatch, event }) => {
   const id = event.id
   const handleClickDeleteButton = () => {
     const message = `イベント(id=${id})を本当に削除しても良いですか？`
     window.confirm(message) &&
-      dispatch({ type: 'DELETE_EVENT', id })
+      dispatch({ type: DELETE_EVENT, id })
   }
 
   return (
